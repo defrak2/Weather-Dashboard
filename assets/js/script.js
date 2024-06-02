1f3d3b7212343db50c92e8a1e652cddea118fdc0
defrak2 <defranciscok22@gmail.com>
2024-06-02 10:59:59 -0400

    trying to fix bugs with API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,110 @@
-const button = document.querySelector('.button')
+const button = document.querySelector('#search-btn')
+const errorText = document.querySelector('.error')
+const inputValue = document.querySelector('.inputValue')
+const currentWeatherDiv = document.querySelector('#display-today')
+const forecastDiv = document.querySelector('#display-forecast')
+const searchHistoryList = document.querySelector('#search-history')
 
-const city = document.querySelector('.city')
-const desc = document.querySelector('.desc')
-const temperature = document.querySelector('.temp')
-const responseText = ''
+
+let searchHistory = []
+
+button.addEventListener('click', fetchWeatherData);
 
 
-button.addEventListener('click', function(){
-  const inputValue = document.querySelector('.inputValue').value
-  const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=1&appid=fd9ef7dbe0229fd31347b03516a3d415`
+const city = inputValue.value.trim();
+// function searchCity(){
+
+//   if (city) {
+//     fetchWeatherData(city);
+//     inputValue.value = '';
+//   }
+// }
+
+function fetchWeatherData(city) {
+
+  const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=fd9ef7dbe0229fd31347b03516a3d415`;
+
+  http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+  
   fetch(geoURL)
-
-  .then(function (response) {
-    if (response.status === 404) {
-      responseText.textContent = 'Result not found'
-      return;
-    } else {
-      return response.json()
-    }
-  })
-  .then(function(response){
-    let lat= response[0].lat;
-    let lon= response[0].lon;
-
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fd9ef7dbe0229fd31347b03516a3d415`)
     .then(function (response) {
-      if (response.status === 404) {
-        responseText.textContent = 'Result not found'
-        return;
-      } else {
-        return response.json()
-      }
+      return response.json();
     })
-    .then(data => {
-
-
-      city.innerHTML = data.city.name;
-      temperature.innerHTML = data.list[0].temp_max;
+    .then(function (data) {
+      console.log(data);
     })
-  })
- 
+  //    {
+  //   // console.log('Response', response);
+  //     // return response.json();
+  // })
+
+    .then(response => {
+      const lat= response[0].lat;
+      const lon= response[0].lon; 
+
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fd9ef7dbe0229fd31347b03516a3d415`)
 
 
-  
-  
+      
+      .then(response => response.json())
+      .then(data => {
+        currentWeatherDiv.data.city.name;      
+        console.log(data.city.name);
+      })
+      .catch(error => {
+        console.log('Error fetching weather data:', error)
+      });
+      // console.log(lat);
+    })
+    .catch(error => {
+      console.log('Error fetching geolocation data:', error)
+    });
+    // searchCity();
+  }
+fetchWeatherData(city);
 
-.catch(err => alert("Incorrect city name!"))
-})
+  // .then(function (response) {
+  //   if (response.status === 404) {
+  //     errorText.textContent = 'Result not found'
+  //     return;
+  //   } else {
+  //     return response.json()
+  //   }
+  // })
+  // .then(function(response){
+   
+    
+//   // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fd9ef7dbe0229fd31347b03516a3d415`)
+//     .then(function (response) {
+//       if (response.status === 404) {
+//         errorText.textContent = 'Result not found'
+//         return;
+//       } else {
+//         return response.json()
+//       }
+//     })
+//     .then(data => {
+
+
+//       city.innerHTML = data.city.name;
+//       // temperature.innerHTML = data.list[0].temp_max;
+//     })
+// })}
+
+
+
+
+
+// .catch(err => alert("Incorrect city name!"))
+// })
+
+
+//1. fetch data from API
+//2. save data items to local storage
+//3. read data items from local storage
+//4. create card for today forecast and append to html
+//5. create cards for 5 day forecast and append to html
+//6. function to render the list
+//7. Function for Search button
+// 8. ready function
