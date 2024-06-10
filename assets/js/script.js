f84c5274f0a52ce952e4b31c2807ab94fd9114c5
defrak2 <defranciscok22@gmail.com>
2024-06-10 10:05:50 -0400

    added fetch data to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('#search-btn')
-const todayBox = document.querySelector('#today-content');
+const todayBox = document.querySelector('#today-content')
+const forecastBox = document.querySelector('#forecast-content')
 const errorText = document.querySelector('.error')
+// const emojiItem = document.querySelector('#emoji-item')
 
-const currentWeatherDiv = document.querySelector('#display-today')
-const forecastDiv = document.querySelector('#display-forecast')
 const searchHistoryList = document.querySelector('#search-history')
 
 
@@ -17,8 +17,8 @@ button.addEventListener('click', function(event){
 
 
 function fetchWeatherData() {
-  const inputValue = document.querySelector('.inputValue')
-  const city = inputValue.value
+  const inputValue = document.querySelector('.inputValue');
+  const city = inputValue.value;
   const geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=fd9ef7dbe0229fd31347b03516a3d415`;
 
 
@@ -42,19 +42,23 @@ function fetchWeatherData() {
 
   }
   function fetchForecast(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fd9ef7dbe0229fd31347b03516a3d415`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=fd9ef7dbe0229fd31347b03516a3d415`)
 
       .then(response => response.json())
       .then(data => {    
         console.log(data);
-        if (!data || !data.results || data.results.length === 0) {
-          console.log('No Results returned!')
-          return;
-      }
-      createTodayCard();
-      data.results.forEach(function(answer) {
-          createForecastCards(answer);
-      })
+      //   if (!data || !data.results || data.results.length === 0) {
+      //     console.log('No Results returned!')
+      //     return;
+      // }
+      createForecastCards(data);
+      createTodayCard(data);
+// for (let i=1; i< 6; i++){
+//   if (data[i])
+// }
+      // data.results.forEach(function(answer) {
+      //     createTodayCard(answer);
+      // })
       })
   }
 function saveToLocalStorage () {
@@ -63,39 +67,68 @@ function saveToLocalStorage () {
 function readFromLocalStorage() {
   //at ready
 }
-function createTodayCard(resultItem) {
+function createTodayCard(data) {
   const todayCard = document.createElement('div');
   todayCard.classList.add('today-card');
 
   const todayCity = document.createElement('h3');
-  todayCity.textContent = resultItem.city.name ;
+  todayCity.textContent = data.city.name + data.list[0].dt_txt;
   //add date here too as well as emoji for sun/cloud/rain
 
   todayCard.append(todayCity);
 
   const todayTemp = document.createElement('p');
-  todayTemp.textContent = resultItem.list.main.temp_max;
+  todayTemp.textContent= data.list[0].main.temp_max;
+
 
   const todayWind = document.createElement('p');
-  todayWind.textContent = resultItem.list.wind.speed;
+  todayWind.textContent = data.list[0].wind.speed;
 
   const todayHumidity = document.createElement('p');
-  todayHumidity.textContent = resultItem.list.main.humidity;
+  todayHumidity.textContent = data.list[0].main.humidity;
 
-  todayWind.append(todayHumidity);
+  todayCard.appendChild(todayTemp);
+  todayCard.appendChild(todayWind);
+  todayCard.appendChild(todayHumidity);
 
-  todayTemp.append(todayWind);
-
-  todayCard.append(todayTemp);
-
-  todayBox.append(todayCard);
+  todayBox.appendChild(todayCard);
 }
-function createForecastCards() {
+function createForecastCards(data) {
 
+
+  for (let i=5; i < data.list.length; i+=8){
+    const forecastCard = document.createElement('div');
+    forecastCard.classList.add('forecast-card');
+  
+    const forecastDescription = document.createElement('h5');
+    forecastDescription.textContent = data.list[i].dt_txt+data.list[i].weather[0].description;
+    //add date here too as well as emoji for sun/cloud/rain add a if statement that creates emojis
+  
+    forecastCard.append(forecastDescription);
+  
+    const forecastTemp = document.createElement('p');
+    forecastTemp.textContent= 'Temp:' + data.list[i].main.temp;
+  
+    const forecastWind = document.createElement('p');
+    forecastWind.textContent = 'Wind:' + data.list[i].wind.speed;
+  
+    const forecastHumidity = document.createElement('p');
+    forecastHumidity.textContent = 'Humidity:' + data.list[i].main.humidity;
+  
+    forecastCard.appendChild(forecastTemp);
+    forecastCard.appendChild(forecastWind);
+    forecastCard.appendChild(forecastHumidity);
+  
+    forecastBox.appendChild(forecastCard);
+  }
+  
 }
+
+
 
 function createCityButtons() {
-  
+  const searchCity = document.createElement('button');
+ //city name here
 }
 //1. fetch data from API
 //2. save data items to local storage
